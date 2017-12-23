dc65c62c3fa37437db2938c3282922c69a88c15a
bipinkh <bipinkh91@gmail.com>
2017-12-23 20:18:24 +0545

    added media contract

--- pixelMessageContract.sol
+++ pixelMessageContract.sol
@@ -53,3 +53,101 @@ contract users{
     }
 }
 
+
+
+contract media{
+     
+    struct mediaDetails{
+        bool isAvailable;   //false if deleted
+        bool isSellable;
+        uint price;
+        address originalAuthor;
+        address currentAuthor;
+    }
+    
+    mapping (bytes32 => mediaDetails) mediaDetailsMap;  //media hash to media details
+    address contractOwner;
+    
+    /* constructor */
+    function media() public{
+        contractOwner = msg.sender;
+    }
+    
+    /* modifiers*/
+    modifier isSellable(bytes32 hash){                                  // if it is available for trade
+        require( mediaDetailsMap[hash].isSellable == true );
+        _;
+    }
+    modifier isAvailable(bytes32 hash){                                  // media exists or is not deleted
+        require( mediaDetailsMap[hash].isAvailable == true );
+        _;
+    }   
+    modifier legalCurrentOwner(bytes32 hash){                           // if the media belongs to the correct current Author
+        require( mediaDetailsMap[hash].currentAuthor == msg.sender);
+        _;
+    }
+    modifier noDuplicate(bytes32 hash){                                  // check if that media already existed
+        require( mediaDetailsMap[hash].isAvailable != true );
+        _;
+    } 
+
+
+
+    /* functions */
+    
+
+    // upload a media
+    function createMedia(bytes32 hash, bool sellable, uint price) public noDuplicate(hash) returns(bool){
+        mediaDetails md = mediaDetailsMap[hash];
+        md.isAvailable = true;
+        md.isSellable = sellable;
+        md.price = price;
+        md.originalAuthor = msg.sender;
+        md.currentAuthor = msg.sender;
+    }
+    
+    //delete a media
+    function deleteMedia(bytes32 hash) public isAvailable(hash) returns(bool){
+        mediaDetails md = mediaDetailsMap[hash];
+        md.isAvailable = false;
+        return true;
+    }
+    
+    //get the price of media
+    function getPrice(bytes32 hash) public isAvailable(hash) isSellable(hash) returns(uint){   
+        return mediaDetailsMap[hash].price;
+    }
+    
+    //get the price of media
+    function getOwner(bytes32 hash) public isAvailable(hash) returns(address){   
+        return mediaDetailsMap[hash].currentAuthor;
+    }
+    
+    //set new price
+    function setNewPrice(bytes32 hash, uint newPrice) public isAvailable(hash) legalCurrentOwner(hash) returns(bool){
+        mediaDetails md = mediaDetailsMap[hash];
+        md.price = newPrice;
+        return true;
+    }
+    
+     //set new sellable status
+    function setSellableStatus(bytes32 hash, bool sellStatus) public isAvailable(hash) legalCurrentOwner(hash) returns(bool){
+        mediaDetails md = mediaDetailsMap[hash];
+        md.isSellable = sellStatus;
+        return true;
+    }
+    
+    //buy the media
+    function buy(bytes32 hash)public payable isAvailable(hash) isSellable(hash) returns(bool){
+        mediaDetails md = mediaDetailsMap[hash];
+        require(msg.value > md.price);              //check if price is enough to buy
+        
+        md.currentAuthor.transfer(msg.value);
+        md.currentAuthor = msg.sender;
+
+        return true;
+    }
+    
+    
+}
+
